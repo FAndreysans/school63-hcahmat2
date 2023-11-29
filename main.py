426063dfe9d884a88809e2536793c6ded78016e1
FAndreysans <melandr2011@gmail.com>
2023-11-29 16:28:54 +0300

    add file

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ t = [[1,2,3,0,0,3,2,1],
      [14,14,14,14,14,14,14,14],
      [11,12,13,0,0,13,12,11]]
 def hod(t):
-    pass
+     print("введите коардинаты фигуры х у")
+     x = int(input())
+     y = int(input())
+     if t[x][y] == 0:
+          print("там пусто...(как в моём кошельке)")
+     if t[x][y] == 4 or t[x][y] == 14:
+          print("введите куда сходить х у")
+          x = int(input())
+          y = int(input())
+          if x + 1 != 0 or x - 1 != 0 or y + 1 != 0 or y - 1 != 0:
+               return False
+          elif x + 1 == 0 and y - 1 == 0 or x + 1 == 0 and y + 1 == 0 or x - 1 == 0 and y - 1 == 0 and x - 1 == 0 and y + 1 == 0:
+               return False
